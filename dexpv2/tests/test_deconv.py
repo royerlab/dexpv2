88a1c612b1af31c2cd7e9b479cc475d08c345c24
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2023-04-10 15:12:07 -0700

    WIP: adding tests to validate deconvolution algorithms

--- dexpv2/tests/test_deconv.py
+++ dexpv2/tests/test_deconv.py
@@ -12,26 +12,22 @@ LOG = logging.getLogger(__name__)
 
 try:
     import cupy as xp
-    import cupyx.scipy.ndimage as ndi
 
     LOG.info("cupy found.")
 
 except (ModuleNotFoundError, ImportError):
     import numpy as xp
-    import scipy.ndimage as ndi
 
     LOG.info("cupy not found using numpy and scipy.")
 
 
-def gaussian_kernel(size: int, sigma: float, ndim: int) -> ArrayLike:
+def gaussian_freq(size: int, sigma: float, ndim: int) -> ArrayLike:
     line = xp.arange(-size // 2 + 1, size // 2 + 1)
 
     assert len(line) == size
-    assert line[size // 2] == 0.0
 
     grid = xp.stack(xp.meshgrid(*[line for _ in range(ndim)]), axis=-1)
-    kernel = xp.exp(-xp.square(grid).sum(axis=-1) / (2 * sigma**2))
-    kernel = kernel / xp.sum(kernel)
+    kernel = xp.exp((-2 * xp.pi * sigma * sigma) * xp.square(grid).sum(axis=-1))
 
     return kernel
 
@@ -41,13 +37,13 @@ def test_deconv_2d(accelerated: bool, display_test: bool) -> None:
     img = xp.asarray(color.rgb2gray(astronaut()), dtype=xp.float32)
 
     iterations = 15 if accelerated else 30
-    kernel = xp.ones((5,) * img.ndim)
+    Fkernel = gaussian_freq(img.shape[0], 0.005, ndim=2)
+    Fimg = xp.fft.fftn(img)
 
-    blurred = (
-        ndi.convolve(img, kernel)
-        + (xp.random.poisson(lam=25, size=img.shape) - 10) / 255.0
-    )
+    blurred = xp.fft.ifftn(Fimg * xp.fft.ifftshift(Fkernel)).real
+    blurred = blurred + (xp.random.poisson(lam=25, size=img.shape) - 10) / 255.0
 
+    kernel = xp.fft.fftshift(xp.fft.ifftn(Fkernel))
     deconv = lucy_richardson(
         blurred,
         kernel,
@@ -55,47 +51,37 @@ def test_deconv_2d(accelerated: bool, display_test: bool) -> None:
         accelerated=accelerated,
     )
 
+    difference = img - deconv
+
     if display_test:
         import napari
 
         viewer = napari.Viewer()
 
-        viewer.add_image(to_cpu(img), blending="additive", colormap="green")
-        viewer.add_image(to_cpu(kernel), blending="additive", colormap="magma")
-        viewer.add_image(to_cpu(blurred), blending="additive", colormap="red")
-        viewer.add_image(to_cpu(deconv), blending="additive", colormap="blue")
+        viewer.add_image(to_cpu(img), blending="additive", colormap="green", name="img")
+        viewer.add_image(
+            to_cpu(Fimg.real), blending="additive", colormap="green", name="img"
+        )
+        viewer.add_image(
+            to_cpu(Fkernel), blending="additive", colormap="magma", name="Fkernel"
+        )
+        viewer.add_image(
+            to_cpu(xp.square(kernel)).real,
+            blending="additive",
+            colormap="magma",
+            name="kernel",
+        )
+        viewer.add_image(
+            to_cpu(blurred), blending="additive", colormap="red", name="blurred"
+        )
+        viewer.add_image(
+            to_cpu(deconv), blending="additive", colormap="blue", name="deconv"
+        )
+        viewer.add_image(
+            to_cpu(difference), blending="additive", colormap="magma", name="difference"
+        )
 
         napari.run()
 
-    # TODO add reconstruction error check
-
-
-# @pytest.mark.parametrize("accelerated", [False, True])
-# def test_deconv_3d(accelerated: bool, display_test: bool) -> None:
-#     data = color.rgb2gray(astronaut)
-#     membrane = xp.asarray(data[:, 0], dtype=xp.float32)
-#     nuclei = xp.asarray(data[:, 1], dtype=xp.float32)
-#
-#     iterations = 10 if accelerated else 20
-#     kernel = gaussian_kernel(5, 1.0, membrane.ndim)
-#
-#     for img in (nuclei, membrane):
-#         blurred = ndi.convolve(img, kernel)
-#         deconv = lucy_richardson(
-#             blurred,
-#             kernel,
-#             iterations,
-#             accelerated=accelerated,
-#         )
-#
-#         if display_test:
-#             import napari
-#             viewer = napari.Viewer()
-#
-#             viewer.add_image(to_cpu(img), blending="additive", colormap="green")
-#             viewer.add_image(to_cpu(kernel), blending="additive", colormap="magma")
-#             viewer.add_image(to_cpu(blurred), blending="additive", colormap="red")
-#             viewer.add_image(to_cpu(deconv), blending="additive", colormap="blue")
-#
-#             napari.run()
-#
+    # TODO define a value of reconstruction error check
+    assert xp.mean(xp.abs(difference)) < 1
