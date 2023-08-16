ef489f7e99726f695c27c57f31ffb0cc1adedeaa
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2023-08-16 14:03:11 -0700

    renaming tiling parameters

--- dexpv2/tiling.py
+++ dexpv2/tiling.py
@@ -1,5 +1,5 @@
 from itertools import product
-from typing import Callable, Optional, Tuple
+from typing import Callable, Optional, Tuple, Union
 
 import numpy as np
 from numpy.typing import ArrayLike
@@ -9,8 +9,8 @@ from dexpv2.cuda import to_numpy
 
 
 def blending_map(
-    tiles: Tuple[int, ...],
-    overlaps: Tuple[int, ...],
+    tile: Tuple[int, ...],
+    overlap: Tuple[int, ...],
     ignore_right: Tuple[int, ...] = tuple(),
 ) -> np.ndarray:
     """
@@ -18,9 +18,9 @@ def blending_map(
 
     Parameters
     ----------
-    tiles : Tuple[int, ...]
+    tile : Tuple[int, ...]
         Sizes of the tiles along each dimension.
-    overlaps : Tuple[int, ...]
+    overlap : Tuple[int, ...]
         Overlaps of tiles along each dimension.
     ignore_right : Tuple[int, ...], optional
         Indices of dimensions to ignore right-side blending, by default an empty tuple.
@@ -31,19 +31,19 @@ def blending_map(
         Blending map for tiles with specified overlaps.
     """
     blending = 1
-    for i in range(len(tiles)):
-        if 2 * overlaps[i] > tiles[i]:
+    for i in range(len(tile)):
+        if 2 * overlap[i] > tile[i]:
             raise ValueError(
-                f"2x overlap {overlaps[i]} cannot be larger than tile size {tiles[i]}."
+                f"2x overlap {overlap[i]} cannot be larger than tile size {tile[i]}."
             )
-        left_border = np.linspace(0, 1, overlaps[i] + 2)[1:-1]
+        left_border = np.linspace(0, 1, overlap[i] + 2)[1:-1]
         if i in ignore_right:
             right_border = np.ones_like(left_border)
         else:
             right_border = np.ones_like(left_border) - left_border
-        line_blending = np.concatenate((left_border, np.ones(tiles[i]), right_border))
+        line_blending = np.concatenate((left_border, np.ones(tile[i]), right_border))
         line_blending = line_blending[
-            (None,) * i + (...,) + (None,) * (len(tiles) - i - 1)
+            (None,) * i + (...,) + (None,) * (len(tile) - i - 1)
         ]
         blending = blending * line_blending
 
@@ -53,8 +53,8 @@ def blending_map(
 def apply_tiled(
     arr: np.ndarray,
     func: Callable[[ArrayLike], ArrayLike],
-    tiles: Tuple[int, ...],
-    overlaps: Tuple[int, ...],
+    tile: Tuple[int, ...],
+    overlap: Union[int, Tuple[int, ...]],
     pad: str = "reflect",
     to_device: Optional[Callable[[ArrayLike], ArrayLike]] = None,
     out_dtype: np.dtype = np.float32,
@@ -68,9 +68,9 @@ def apply_tiled(
         Input array to be tiled and processed.
     func : Callable[[ArrayLike], ArrayLike]
         Function to apply to each tile.
-    tiles : Tuple[int, ...]
+    tile : Tuple[int, ...]
         Sizes of the tiles along each dimension.
-    overlaps : Tuple[int, ...]
+    overlap : Union[int, Tuple[int, ...]]
         Overlaps of tiles along each dimension.
     pad : str, optional
         Padding mode for tiling, by default "reflect".
@@ -84,43 +84,48 @@ def apply_tiled(
     np.ndarray
         Output array with the function applied to tiled portions.
     """
+    if isinstance(overlap, int):
+        overlap = (overlap,) * len(tile)
 
-    if len(tiles) != len(overlaps):
+    if len(tile) != len(overlap):
         raise ValueError(
-            f"The length of tiles and overlaps must be the same. Got {len(tiles)} and {len(overlaps)}."
+            f"The length of tiles and overlaps must be the same. Got {len(tile)} and {len(overlap)}."
         )
 
-    num_non_tiled = len(arr.shape) - len(tiles)
+    num_non_tiled = len(arr.shape) - len(tile)
     orig_shape = arr.shape[num_non_tiled:]
     pad_width = ((0, 0),) * num_non_tiled + tuple(
-        (overlap, overlap) for overlap in overlaps
+        (overlap, overlap) for overlap in overlap
     )
 
     arr = np.pad(arr, pad_width, mode=pad)
     out_arr = None
 
-    blending = blending_map(tiles, overlaps)
+    blending = blending_map(tile, overlap)
     if to_device is not None:
         blending = to_device(blending)
 
-    tiling_start = product(
-        *[
-            range(o, size + 2 * o, t + o)
-            for size, t, o in zip(orig_shape, tiles, overlaps)
-        ]
+    tiling_start = list(
+        product(
+            *[
+                range(o, size + 2 * o, t + o)
+                for size, t, o in zip(orig_shape, tile, overlap)
+            ]
+        )
     )
 
     for start_indices in tqdm(tiling_start, "Applying function to tiles"):
         slicing = (...,) + tuple(
             slice(start - o, start + t + o)
-            for start, t, o in zip(start_indices, tiles, overlaps)
+            for start, t, o in zip(start_indices, tile, overlap)
         )
 
-        tile = arr[slicing]
+        in_tile = arr[slicing]
         if to_device is not None:
-            tile = to_device(tile)
+            in_tile = to_device(in_tile)
 
-        out_tile = func(tile)
+        out_tile = func(in_tile)
+        del in_tile
 
         if out_arr is None:
             out_arr = np.zeros(
@@ -130,15 +135,15 @@ def apply_tiled(
 
         short_axes = tuple(
             i
-            for i in range(len(tiles))
+            for i in range(len(tile))
             if out_tile.shape[i + num_non_tiled] != blending.shape[i]
         )
 
         if len(short_axes) > 0:
             # ignoring right side blending on axes that the last tile doesn't overhang
             fixed_blending = blending_map(
-                tiles,
-                overlaps,
+                tile,
+                overlap,
                 ignore_right=short_axes,
             )
             if to_device is not None:
@@ -153,12 +158,10 @@ def apply_tiled(
             out_tile = out_tile * blending
 
         out_tile = to_numpy(out_tile)
-
         out_arr[slicing] += out_tile
+        del out_tile
 
-    slicing = (...,) + tuple(
-        slice(o, size + o) for size, o in zip(orig_shape, overlaps)
-    )
+    slicing = (...,) + tuple(slice(o, size + o) for size, o in zip(orig_shape, overlap))
     if out_arr is None:
         raise ValueError("No tiles were processed.")
 
