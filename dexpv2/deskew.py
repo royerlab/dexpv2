e382b877587083675065938ee1ad0bbe14e488f9
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2023-10-18 23:10:17 -0700

    implemented loop-based deskweing

---
+++ dexpv2/deskew.py
@@ -0,0 +1,99 @@
+import math
+import numpy as np
+
+from numpy.typing import ArrayLike
+from tqdm import tqdm
+
+
+def deskewing_shift(
+    angle: float,
+    z_step: float,
+    x_res: float,
+) -> float:
+    """
+    Calculate the shift required for deskewing.
+
+    Parameters
+    ----------
+    angle : float
+        The angle in degrees to be deskewed.
+    z_step : float
+        The z-step size.
+    x_res : float
+        The x-axis resolution in the units as `z_step`.
+
+    Returns
+    -------
+    float
+        The calculated shift.
+    """
+    return np.cos(np.deg2rad(angle)) * z_step / x_res
+
+
+def get_deskewed_shape(
+    shape: tuple,
+    shift: float,
+) -> tuple:
+    """
+    Calculate the new shape after deskewing.
+
+    Parameters
+    ----------
+    shape : tuple
+        The original shape of the array (z, y, x).
+    shift : float
+        The calculated shift for deskewing.
+
+    Returns
+    -------
+    tuple
+        The new shape after deskewing.
+    """
+    assert len(shape) == 3
+
+    shape = list(shape)
+    offset = int(math.fabs(shift) * shape[0] + 0.5)
+    shape[-1] += offset
+
+    return tuple(shape)
+
+
+def deskew(
+    raw_data: ArrayLike,
+    shift: float,
+) -> ArrayLike:
+    """
+    Perform the deskewing operation on the raw data.
+
+    Parameters
+    ----------
+    raw_data : ArrayLike
+        The raw 3D data array with shape (z, y, x).
+    shift : float
+        The calculated shift for deskewing.
+
+    Returns
+    -------
+    ArrayLike
+        The deskewed 3D data array.
+    """
+    new_shape = get_deskewed_shape(raw_data.shape, shift)
+
+    offset = new_shape[-1] - raw_data.shape[-1]
+    out_data = np.zeros(new_shape, dtype=raw_data.dtype, like=raw_data)
+
+    x_length = raw_data.shape[-1] - 1
+    w1 = shift - math.floor(shift)
+    w2 = math.ceil(shift) - shift
+
+    if shift > 0:
+        offset = 0
+
+    for z in tqdm(range(new_shape[0])):
+        current_shift = int(math.floor(z * shift)) + offset
+        out_data[z, :, current_shift : current_shift + x_length] = (
+            w1 * raw_data[z, :, :-1] +
+            w2 * raw_data[z, :, 1:]
+        )
+
+    return out_data
