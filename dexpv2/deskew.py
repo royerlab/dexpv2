743e49ade3abe1b33e7d17bb5500d4a0f40905e9
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2024-03-07 10:11:01 -0800

    working version of yang deskewing, need to fix tests

--- dexpv2/deskew.py
+++ dexpv2/deskew.py
@@ -1,69 +1,29 @@
 import math
+from typing import Callable, Tuple
 
 import numpy as np
 from numpy.typing import ArrayLike
-from tqdm import tqdm
+
+from dexpv2.cuda import import_module, to_numpy
 
 
-def deskewing_shift(
-    angle: float,
-    scan_step: float,
+def _scanning_and_scale(
     pixel_size: float,
-) -> float:
-    """
-    Calculate the shift required for deskewing.
-
-    TILT AXIS
-        /
-       /
-      /
-     /
-    / o ANGLE
-    -------------
-      SCAN AXIS
-
-    Parameters
-    ----------
-    angle : float
-        The angle in degrees to be deskewed.
-    scan_step : float
-        The scan step size along tile axis.
-    pixel_size : float
-        The pixel size (resolution).
-
-    Returns
-    -------
-    float
-        The calculated shift.
-    """
-    return np.cos(np.deg2rad(angle)) * pixel_size / scan_step
-
-
-def deskewed_dimension(
-    angle: float,
     scan_step: float,
-) -> float:
-    """
-    Calculate the length of the deskewed image (z-axis) opposite to angle.
-
-    Parameters
-    ----------
-    angle : float
-        The angle in degrees to be deskewed.
-    scan_step : float
-        The scan_step size.
-
-    Returns
-    -------
-    float
-        The calculated length.
-    """
-    return np.sin(np.deg2rad(angle)) * scan_step
+    tilt_angle: float,
+) -> Tuple[int, float]:
+    """Compute the resample factor and lateral scaling for deskewing."""
+    x_res = pixel_size * math.cos(math.radians(tilt_angle))
+    resample_factor = int(round(scan_step / x_res))
+    lateral_scaling = math.cos(math.radians(tilt_angle))
+    return resample_factor, lateral_scaling
 
 
 def get_deskewed_shape(
     shape: tuple,
-    shift: float,
+    pixel_size: float,
+    scan_step: float,
+    tilt_angle: float,
 ) -> tuple:
     """
     Calculate the new shape after deskewing.
@@ -83,62 +43,132 @@ def get_deskewed_shape(
     if len(shape) != 3:
         raise ValueError(f"The shape must be 3D. Got {shape}.")
 
-    offset = int(math.fabs(shift) * shape[0] + 0.5)
+    resample_factor, lateral_scaling = _scanning_and_scale(
+        pixel_size, scan_step, tilt_angle
+    )
 
-    shape_list = list(shape)
-    shape_list[-1] += offset
+    x_size = int(round((shape[0] * resample_factor + shape[2]) * lateral_scaling))
 
-    return tuple(shape_list)
+    out_shape = (
+        shape[2] // resample_factor - 1,
+        shape[1],
+        x_size,
+    )
+
+    return out_shape
 
 
-def deskew(
-    raw_data: ArrayLike,
-    shift: float,
+def _deskew(
+    crop: ArrayLike,
+    resample_factor: int,
+    lateral_scaling: float,
 ) -> ArrayLike:
     """
-    Perform the deskewing operation on the raw data.
-    Skewing is performed along the xz-slice.
+    Bin Yang deskew.
+    Deskew a volume with an angle between the x and z axis.
+    Reference: https://github.com/royerlab/dexp/blob/master/dexp/processing/deskew/yang_deskew.py
 
     Parameters
     ----------
-    raw_data : ArrayLike
-        The raw 3D data array with shape (z, y, x).
-    shift : float
-        The calculated shift for deskewing.
+    crop : ArrayLike
+        The volume to be deskewed.
+    resample_factor : int
+        Step size for resampling (deskewing).
+    lateral_scaling : float
+        The pixel size lateral scaling.
 
     Returns
     -------
     ArrayLike
-        The deskewed 3D data array.
+        The deskewed volume.
     """
-    new_shape = get_deskewed_shape(raw_data.shape, shift)
-    out_data = np.zeros(new_shape, dtype=raw_data.dtype, like=raw_data)
+    ndi = import_module("scipy", "ndimage")
 
-    x_length = raw_data.shape[-1] - 1
+    (nz, ny, nx) = crop.shape
+    dtype = crop.dtype
 
-    if shift > 0:
-        offset = 0
-    else:
-        offset = new_shape[-1] - raw_data.shape[-1]
+    nz_new, ny_new, nx_new = (
+        len(range(0, nx, resample_factor)),
+        ny,
+        nx + nz * resample_factor,
+    )
+    data_reassign = np.zeros((nz_new, ny_new, nx_new), dtype=dtype, like=crop)
 
-    shift_per_z = np.arange(new_shape[0]) * shift + offset
+    for x in range(nx):
+        x_start = x
+        x_end = nz * resample_factor + x
+        data_reassign[x // resample_factor, :, x_start:x_end:resample_factor] = crop[
+            :, :, x
+        ].T
+    del crop
 
-    # it's inverted because we work at the pixel edge level and not it's center
-    #    p    q    t
-    #    |    |    |
-    # so pixel q will look at values between p, q and q, t
-    # where w_right is the offset from p to q (going to right)
-    # and w_left is the offset from t to q (going to left)
-    w_right = np.ceil(shift_per_z) - shift_per_z
-    w_left = 1.0 - w_right
+    # rescale the data, interpolate along z
+    data_rescale = ndi.zoom(data_reassign, zoom=(resample_factor, 1, 1), order=1)
+    del data_reassign
 
-    x_length = raw_data.shape[-1] - 1
-    int_shift = np.ceil(shift_per_z).astype(int)
+    data_interp = np.zeros((nz_new, ny_new, nx_new), dtype=dtype, like=data_rescale)
 
-    for z in tqdm(range(new_shape[0])):
-        current_shift = int_shift[z]
-        out_data[z, :, current_shift : current_shift + x_length] = (
-            w_left[z] * raw_data[z, :, :-1] + w_right[z] * raw_data[z, :, 1:]
+    for z in range(nz_new):
+        for k in range(resample_factor):
+            data_interp[z, :, k::resample_factor] = data_rescale[
+                z * resample_factor - k, :, k::resample_factor
+            ]
+    del data_rescale
+
+    # rescale the data, to have voxel the same along x an y;
+    # remove the first z slice which has artifacts due to resampling
+    image_final = ndi.zoom(data_interp[1:], zoom=(1, 1, lateral_scaling), order=1)
+
+    return image_final
+
+
+def deskew(
+    raw_data: ArrayLike,
+    pixel_size: float,
+    scan_step: float,
+    tilt_angle: float,
+    num_splits: int = 4,
+    to_device: Callable[[ArrayLike], ArrayLike] = lambda x: x,
+) -> np.ndarray:
+    """
+    Deskew a volume with an angle between the x and z axis.
+    Using Bin Yang deskew.
+    The deskewing is done per chunks in y-axis.
+    Reference: https://github.com/royerlab/dexp/blob/master/dexp/processing/deskew/yang_deskew.py
+
+    Parameters
+    ----------
+    raw_data : ArrayLike
+        The volume to be deskewed
+    pixel_size : float
+        The pixel size (dx resolution)
+    scan_step : float
+        The acquisition scan step size
+    tilt_angle : float
+        The angle in degrees to be deskewed.
+    num_splits : int, optional
+        Number of splits in y-axis
+    to_device : Callable[[ArrayLike], ArrayLike], optional
+        Function to move data to device
+
+    Returns
+    -------
+    np.ndarray
+        The deskewed volume.
+    """
+    resample_factor, lateral_scaling = _scanning_and_scale(
+        pixel_size, scan_step, tilt_angle
+    )
+
+    deskewed_crops = [
+        to_numpy(
+            _deskew(
+                to_device(crop),
+                resample_factor,
+                lateral_scaling,
+            )
         )
+        for crop in np.array_split(raw_data, num_splits, axis=1)
+    ]
 
-    return out_data
+    return np.concatenate(deskewed_crops, axis=1)
