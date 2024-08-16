708b99b6aea016ffc8fc588687216c272887215b
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2024-08-15 17:33:52 -0700

    fix bbox tests

--- dexpv2/_tests/test_crop.py
+++ dexpv2/_tests/test_crop.py
@@ -7,8 +7,10 @@ from dexpv2.crop import find_moving_bboxes_consensus, fix_crop_slice, to_slice
 def test_find_moving_bboxes_consensus_normal() -> None:
     bboxes = np.array([[0, 0, 10, 10], [2, 2, 12, 12]])
     shifts = np.array([[1, 1], [1, 1]])
-    expected_output = np.array([[1, 1, 11, 11], [2, 2, 12, 12]])
-    output = find_moving_bboxes_consensus(bboxes, shifts)
+    expected_output = np.array(
+        [[0, 0, 10, 10, 0, 0, 10, 10], [2, 2, 12, 12, 1, 1, 11, 11]]
+    )
+    output = find_moving_bboxes_consensus(bboxes, shifts, shape=(14, 14))
     np.testing.assert_array_equal(output, expected_output)
 
 
@@ -16,14 +18,14 @@ def test_find_moving_bboxes_consensus_mismatch_error() -> None:
     bboxes = np.array([[0, 0, 10, 10]])
     shifts = np.array([[1, 1], [1, 1]])
     with pytest.raises(ValueError):
-        find_moving_bboxes_consensus(bboxes, shifts)
+        find_moving_bboxes_consensus(bboxes, shifts, shape=(12, 12))
 
 
 def test_find_moving_bboxes_consensus_dimension_error() -> None:
     bboxes = np.array([[0, 0, 10]])
     shifts = np.array([[1, 1]])
     with pytest.raises(ValueError):
-        find_moving_bboxes_consensus(bboxes, shifts)
+        find_moving_bboxes_consensus(bboxes, shifts, shape=(12, 12))
 
 
 def test_to_slice() -> None:
